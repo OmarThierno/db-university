31c81a678e76aff6943677315204d4111ebe1338
OmarThierno <thiernomarndiaye76@gmail.com>
2024-05-29 18:58:09 +0200

    feat: add milestone 4, 5, 6, 7, 8)

--- response.sql
+++ response.sql
@@ -14,9 +14,39 @@ SELECT *
 FROM `courses`
 WHERE `cfu` > 10;
 
--- 3. Selezionare tutti gli studenti che hanno più di 30 anni
+-- 3. Selezionare tutti gli studenti che hanno più di 30 anni (3799)
 
 SELECT * 
 FROM `students`
-WHERE YEAR(`date_of_birth`) = 2024 - 30
-AND MONTH (`date_of_birth`) < 5;
+WHERE YEAR(`date_of_birth`) <= 2024 - 30;
+
+-- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+SELECT * FROM `courses`
+WHERE `period` = "I semestre"
+AND `year`= 1;
+
+-- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+
+SELECT * 
+FROM `exams` 
+WHERE `date` = "2020-06-20"
+AND HOUR(`hour`) >= 14;
+
+-- 6. Selezionare tutti i corsi di laurea magistrale (38)
+
+SELECT * 
+FROM `degrees` 
+WHERE `level` = "magistrale";
+
+-- 7. Da quanti dipartimenti è composta l'università? (12)
+
+SELECT count(`id`) AS `number_departments`
+FROM `departments`;
+
+-- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+
+SELECT * 
+FROM `teachers`
+WHERE `phone` IS NULL;
+
+
